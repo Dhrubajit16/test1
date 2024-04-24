2c992cbcb6ee8b7d9132b06c1bed66af2b8535ea
Dhrubajit Kashyap <kashyapdhrubajit1@gmail.com@gmail.com>
2024-04-24 10:07:33 +0530

    feature signup working

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+
+import AppRoutes from "./AppRoutes";
+import Header from "./components/Header/Header";
+//import Thumbnails from "./components/Thumbnails/Thumbnails";
+function App() {
+  return (
+   
+      <>
+      
+      <Header/>
+      
+      
+      <AppRoutes/>
+      
+      
+      </>
+
+
+
+
+
+  );
+
+
+
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,114 @@
+export const sample_foods = [
+
+
+    {
+
+       id: '1',
+       name: 'Pizza Pepperoni',
+       cookTime:'10-20',
+       price: 10,
+       favorite: false,
+       origins: ['India'],
+       stars:4.5,
+       imageUrl: 'food-1.jpg',
+       tags: ['FastFood','Pizza','Lunch'],
+    },
+
+    {
+        id: '2',
+       name: 'Meatball',
+       cookTime:'20-30',
+       price: 20,
+       favorite: true,
+       origins: ['India'],
+       stars:4.5,
+       imageUrl: 'food-2.jpg',
+       tags: ['SlowFood','Lunch'],
+    },
+
+    {
+    id: '3',
+    name: 'Alu',
+    cookTime:'10-15',
+    price: 15,
+    favorite: true,
+    origins: ['india'],
+    stars:4.5,
+    imageUrl: 'food-3.jpg',
+    tags: ['SlowFood','Lunch'],
+ },
+
+ {
+    id: '3',
+    name: 'Alu',
+    cookTime:'10-15',
+    price: 15,
+    favorite: true,
+    origins: ['india'],
+    stars:4.5,
+    imageUrl: 'food-4.jpg',
+    tags: ['SlowFood','Lunch'],
+ },
+ {
+    id: '3',
+    name: 'Alu',
+    cookTime:'10-15',
+    price: 15,
+    favorite: true,
+    origins: ['india'],
+    stars:4.5,
+    imageUrl: 'food-5.jpg',
+    tags: ['SlowFood','Lunch'],
+ },
+ {
+    id: '3',
+    name: 'Alu',
+    cookTime:'10-15',
+    price: 15,
+    favorite: true,
+    origins: ['india'],
+    stars:4.5,
+    imageUrl: 'food-6.jpg',
+    tags: ['SlowFood','Lunch'],
+ },
+
+
+
+];
+export const sample_tags = [
+   
+   { name:'All',coumt:6},
+   { name:'FastFood',coumt:4},
+   { name:'Pizza',coumt:2},
+   { name:'Lunch',coumt:3},
+   { name:'SlowFood',coumt:2},
+   { name:'Hamburger',coumt:1},
+   { name:'Fry',coumt:1},
+   { name:'Soup',coumt:1},
+   
+   
+];
+
+export const sample_users = [
+   {
+      id: 1,
+      name: 'Jit',
+      email:'jit@gmail.com',
+      password:8901,
+      address:'Assam',
+      isAdmin:false,
+   
+   },
+   
+   {
+      id: 2,
+      name: 'Dhruba',
+      email:'dhruba@gmail.com',
+      password:5678,
+      address:'Assam',
+      isAdmin:true,
+   
+   },
+   
+   ];
+   
